01f549ed507fca48275444ecc45a787f4f51a199
Mageshwaran Murugaian <mageshwaran@lifebit.ai>
2023-04-22 01:53:13 +0530

    Fix path notion problem

--- plugins/nf-azure/src/main/nextflow/cloud/azure/nio/LifebitAzFileSystemProvider.groovy
+++ plugins/nf-azure/src/main/nextflow/cloud/azure/nio/LifebitAzFileSystemProvider.groovy
@@ -13,6 +13,7 @@ class LifebitAzFileSystemProvider extends AzFileSystemProvider {
 
     public static final String AZURE_IS_MULTI_STORAGE_ENABLED = 'AZURE_IS_MULTI_STORAGE_ENABLED'
 
+    private final Map<String,String> mapping = [:]
 
     LifebitAzFileSystemProvider() {
         super()
@@ -25,24 +26,26 @@ class LifebitAzFileSystemProvider extends AzFileSystemProvider {
 
     @Override
     FileSystem getFileSystem(URI uri) {
-        final accountName = getAccountName(uri)
-        final bucket = getContainerName(uri)
-        getFileSystem0("$accountName::$bucket",false)
+        final bucket = this.getContainerName(uri)
+        mapping.put(bucket, this.getAccountName(uri))
+        getFileSystem0("$bucket",false)
     }
 
 
     @Override
-    protected AzFileSystem getFileSystem0(String key, boolean canCreate) {
-        println("LifebitAzFileSystemProvider getFileSystem0 called with Key: $key")
-        def fs = fileSystems.get(key)
+    protected AzFileSystem getFileSystem0(String bucket, boolean canCreate) {
+        println("LifebitAzFileSystemProvider getFileSystem0 called with bucket: $bucket")
+        def accountName = mapping.get(bucket, null)
+        def fs = fileSystems.get(bucket)
         if( !fs ) {
             if( canCreate ) {
                 def config = this.env.deepClone()
-                config.put(AZURE_STORAGE_ACCOUNT_NAME, key.split('::')[1])
-//                config.put(AZURE_STORAGE_CONTAINER_NAME, getContainerName2(uri))
-                fs = newFileSystem0(key, config)
+
+                if(accountName)
+                    config.put(AZURE_STORAGE_ACCOUNT_NAME, accountName)
+                fs = newFileSystem0(bucket, config)
             } else {
-                throw new FileSystemNotFoundException("Missing Azure storage blob file system for Key: `$key`")
+                throw new FileSystemNotFoundException("Missing Azure storage blob file system for bucket: `$bucket`")
             }
         }
 
@@ -54,51 +57,47 @@ class LifebitAzFileSystemProvider extends AzFileSystemProvider {
         println("LifebitAzFileSystemProvider newFileSystem called with bucekt ${uri.authority} - $config")
         final bucket = getContainerName(uri)
         final accountName = getAccountName(uri)
+        mapping.put(bucket, accountName)
         def config2 = config.deepClone()
-        config2.put(AZURE_STORAGE_ACCOUNT_NAME, accountName)
-        newFileSystem0("$accountName::$bucket", config2)
+
+        if(accountName)
+            config2.put(AZURE_STORAGE_ACCOUNT_NAME, accountName)
+        newFileSystem0("$bucket", config2)
     }
 
     @Override
-    protected AzFileSystem createFileSystem(BlobServiceClient client, String key, Map<String,?> config) {
-        println("LifebitAzFileSystemProvider createFileSystem $key")
-        def result = new AzFileSystem(this, client, key.split('::').length > 1? key.split('::')[1] : key)
+    protected AzFileSystem createFileSystem(BlobServiceClient client, String bucket, Map<String,?> config) {
+        println("LifebitAzFileSystemProvider createFileSystem $bucket")
+        def result = new AzFileSystem(this, client, bucket)
         return result
     }
 
     protected String getAccountName(URI uri) {
-        return super.getContainerName(uri)
+        final name = super.getContainerName(uri)
+        return !name.contains('.') ? null: name.split('\\.')[0]
     }
 
     @Override
     protected String getContainerName(URI uri) {
-        return super.getContainerName(new URI(SCHEME, uri.path.substring(1), uri.query, uri.fragment))
+        final name = super.getContainerName(uri)
+        return !name.contains('.') ? name: name.split('\\.')[1]
     }
 
-//    @Override
-//    AzPath getPath(URI uri) {
-//        println("LifebitAzFileSystemProvider getPath Uri $uri")
-//        final bucket = this.getContainerName(uri)
-//        bucket == '/' ? getPath('/') : getPath("${getAccountName(uri)}/${uri.path}")
-//    }
+    @Override
+    AzPath getPath(URI uri) {
+        println("LifebitAzFileSystemProvider getPath Uri $uri")
+        final bucket = this.getContainerName(uri)
+        bucket == '/' ? getPath('/') : getPath("${bucket}/${uri.path}")
+    }
 
     @Override
     AzPath getPath(String path) {
         println("LifebitAzFileSystemProvider getPath $path")
 
-        /** GET ACCOUNT NAME */
-        while( path.startsWith("/") )
-            path = path.substring(1)
-
-        // -- find the first component ie. the container name
-        URI uri = new URI("$SCHEME://$path")
-        final accountName = uri.authority
-        path = uri.path
-        /** GET ACCOUNT NAME */
 
         // -- special root bucket
         if( path == '/' ) {
-            final fs = getFileSystem0("$accountName::/",true)
+            final fs = getFileSystem0("/",true)
             return new AzPath(fs, "/")
         }
 
@@ -108,10 +107,16 @@ class LifebitAzFileSystemProvider extends AzFileSystemProvider {
 
         // -- find the first component ie. the container name
         int p = path.indexOf('/')
-        final bucket = p==-1 ? path : path.substring(0,p)
+        String bucket = p==-1 ? path : path.substring(0,p)
 
+        //multiple storage account support
+        if(bucket.contains('.')) {
+            bucket = bucket.split('\\.')[1]
+            path = bucket + path.substring(path.indexOf('/'))
+
+        }
         // -- get the file system
-        final fs = getFileSystem0("$accountName::$bucket",true)
+        final fs = getFileSystem0("$bucket",true)
 
         // create a new path
         new AzPath(fs, "/$path")
